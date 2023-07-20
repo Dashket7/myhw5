7afbe7750b2c6691ec48d1d3446142eb87f6f029
Artemeva D <dashartemeva7@gmail.com>
2023-07-20 13:44:00 +0500

    ParametrizedTest commit

--- src/test/java/ru/netology/services/CountServiceTest.java
+++ src/test/java/ru/netology/services/CountServiceTest.java
@@ -2,13 +2,17 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CountServiceTest {
-    @Test
-    public void calcCount() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/count.csv")
+    public void calcCount(int expected,int income, int expenses, int threshold) {
         CountService service = new CountService();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+        //int expected = 2;
+        int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 }
